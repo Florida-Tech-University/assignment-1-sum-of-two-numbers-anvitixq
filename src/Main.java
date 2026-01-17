b090fd5302b51c67313aff40cb4f8f4a742cbaaf
Anviti Alok <anviti17@gmail.com>
2026-01-17 16:57:15 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,11 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
 
-        // TODO: Read two integers
-      
-
-        // TODO: Print their sum  using println
+        System.out.println(a + b);
       
         scanner.close();
     }
